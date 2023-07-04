2016aa8c59c40018e9e7e4160e2d6f14b9f94eee
leo-rp <leorock1988@hotmail.com>
2023-07-04 02:49:15 -0500

    - added a frame demo - added sky element

---
+++ src/components/AframeDemo.vue
@@ -0,0 +1,23 @@
+<script setup>
+import 'aframe';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
+
+const loader = new GLTFLoader();
+// src="./assets/logo.svg
+
+</script>
+
+<template>
+	<a-scene>
+		<a-assets>
+			<img id="sky" src="./assets/stars.jpg" />		
+		</a-assets>
+		
+		<a-sky>
+			color = "FFFFFF"
+			material = "src: #sky"
+			rotation = "0 0 0"			
+		</a-sky>
+	</a-scene>	
+</template>
