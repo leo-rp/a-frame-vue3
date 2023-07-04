bb62d9d9c6f96fb544a5f5101941d37f4fadfeb4
leo-rp <leorock1988@hotmail.com>
2023-07-04 03:28:06 -0500

    - added mountain

--- src/components/AframeDemo.vue
+++ src/components/AframeDemo.vue
@@ -1,23 +1,31 @@
 <script setup>
 import 'aframe';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import sky from '@/assets/stars.jpg'
+import mountain from '@/assets/models/mountain.glb';
+
 
 
 const loader = new GLTFLoader();
-// src="./assets/logo.svg
+loader.load(mountain, (d) => {
+	const entity = document.getElementById("mountain");
+	entity.object3D.add(d.scene);
+});
 
 </script>
 
 <template>
 	<a-scene>
 		<a-assets>
-			<img id="sky" src="./assets/stars.jpg" />		
+			<img id="sky" :src="sky"/>
 		</a-assets>
 		
-		<a-sky>
-			color = "FFFFFF"
-			material = "src: #sky"
-			rotation = "0 0 0"			
-		</a-sky>
+		<a-sky
+			color= "#e2e2e2"
+			material="src: #sky"
+			rotation = "0 0 0"
+		/>
+
+		<a-entity id="mountain" position="0 0 0" scale="10 10 10"/>
 	</a-scene>	
 </template>
