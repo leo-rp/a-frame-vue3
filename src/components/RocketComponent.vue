da68fe838663ddf7a374f5f83a4b683b259e0a71
leo-rp <leorock1988@hotmail.com>
2023-07-04 04:14:28 -0500

    - added rocket component

---
+++ src/components/RocketComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import 'aframe';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import rocketModel from '@/assets/models/rocket.glb';
+
+const props = defineProps({
+	x: Number,
+	y: Number,
+	z: Number
+})
+
+const loader = new GLTFLoader();
+loader.load(rocketModel, (d) => {
+	const entity = document.getElementById("rocket");
+	entity.object3D.add(d.scene);
+});
+
+
+</script>
+
+<template>
+	<a-entity id="rocket" :position="`${props.x} ${props.y} ${props.z}`" scale="15 15 15"/>	
+</template>
